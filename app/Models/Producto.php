217e332d4d3ee6b23e34608720959fab331f46ce
Margoth <diany.lopez@unah.hn>
2025-02-16 20:36:11 -0600

    Relación entre productos y categorías

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Producto extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = ['nombre','descripcion','precio','categoria_id','stock','imagen'];
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
 }
