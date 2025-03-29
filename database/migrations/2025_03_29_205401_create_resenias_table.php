3667561f85cbee3c8071ca0b3fffb9f5c220912e
Margoth <diany.lopez@unah.hn>
2025-03-29 14:57:31 -0600

    migración de reseñas agregado

--- database/migrations/2025_03_29_205401_create_resenias_table.php
+++ database/migrations/2025_03_29_205401_create_resenias_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('resenias', function (Blueprint $table) {
             $table->id();
-$table->string('titulo');
-$table->string('contenido');
-$table->foreignId('user_id');
-$table->timestamps();
-$table->softDeletes();//
+            $table->string('titulo');
+            $table->string('contenido');
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
