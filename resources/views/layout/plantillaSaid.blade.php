9e6d640e4e04327c1983d24a5762e42d6262915e
Said Artica <said.artica@unah.hn>
2025-04-14 01:44:21 -0600

    Refactor VeterinariaController, Ubicacion, and Veterinaria models; actulizacion vistas Added latitude and longitude fields, adjusted pagination, and enhanced user interface elements.

--- resources/views/layout/plantillaSaid.blade.php
+++ resources/views/layout/plantillaSaid.blade.php
@@ -7,10 +7,28 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <style>
+        body {
+            padding-top: 90px;
+        }
+        @media (max-width: 768px) {
+            body {
+                padding-top: 56px;
+            }
+        }
+        @media(orientation: landscape) and (max-width: 768px) {
+            body {
+                padding-top: 79px;
+            }
+        }
+    </style>
 </head>
+
 <body>
 @include('MenuPrincipal.Navbar')
-    <div class="container mx-auto mt-5 pt-5">
+    <div class="container mx-auto mt-3 ">
         @yield("contenido")
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
-
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
