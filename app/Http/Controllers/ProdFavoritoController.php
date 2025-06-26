d3c66c1048da1b262297980a1693df6a1c495d17
Margoth <diany.lopez@unah.hn>
2025-06-25 22:28:04 -0600

    Se agregó la función index en el controlador de producto favorito

--- app/Http/Controllers/ProdFavoritoController.php
+++ app/Http/Controllers/ProdFavoritoController.php
@@ -6,7 +6,9 @@ class ProdFavoritoController extends Controller
 {
     public function index()
     {
-
+        return view('productos.productos-guardados', [
+            'prod_favoritos' => \App\Models\Prod_favorito::all(),
+        ]);
     }
 
     public function create()
