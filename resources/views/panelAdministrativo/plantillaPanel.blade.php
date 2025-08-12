0b1bee9973b533e8d980ee8ae95eecf91f6cde11
LilianaGarc <lngarciag@unah.hn>
2025-08-12 03:26:46 -0600

    Iconos y bootstrap sin conexion a internet en la plantilla del panel

--- resources/views/panelAdministrativo/plantillaPanel.blade.php
+++ resources/views/panelAdministrativo/plantillaPanel.blade.php
@@ -6,9 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Panel Administrativo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset(path: 'css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset(path: 'css/all.min.css') }}">
     <style>
         :root{
             --background-color: #f7f7f7;
@@ -34,6 +33,7 @@
             height: 100%;
             align-items: center;
             justify-content: center;
+            overflow-x: hidden;
         }
         header{
             z-index: 200;
@@ -809,6 +809,7 @@
     });
 </script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+<script src="{{ asset(path: 'js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset(path: 'js/jquery.min.js') }}"></script>
+<script src="{{ asset(path: 'js/popper.min.js') }}"></script></body>
 </html>
