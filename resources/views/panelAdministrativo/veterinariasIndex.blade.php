4d418f7dcb35c1d3048ee1f7d69e9ba33cf30c93
LilianaGarc <lngarciag@unah.hn>
2025-08-12 03:28:53 -0600

    Edicion de vista en el panel

--- resources/views/panelAdministrativo/veterinariasIndex.blade.php
+++ resources/views/panelAdministrativo/veterinariasIndex.blade.php
@@ -52,7 +52,7 @@
 
                     <div class="dropdown">
                         <button class="btn btn-light dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Actions
+                            Acciones
                         </button>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="{{ route('veterinarias.panelshow', ['id' => $veterinaria->id]) }}">Detalles</a></li>
